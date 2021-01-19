b545e8bc9cd6abc4e165deeecc24e711561dc654
joestilin <jstilin@gmail.com>
2021-01-19 02:20:39 +0000

    Exclusive memory ownership adjustments.
    GraphNode exclusively owns outgoing (child)
    instances of GraphEdges.

--- src/graphnode.h
+++ src/graphnode.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include <string>
 #include "chatbot.h"
+#include <memory>
 
 
 // forward declarations
@@ -16,8 +17,8 @@ private:
     ////
 
     // data handles (owned)
-    std::vector<GraphEdge *> _childEdges;  // edges to subsequent nodes
-
+    std::vector<std::unique_ptr<GraphEdge>> _childEdges;  // edges to subsequent nodes
+    
     // data handles (not owned)
     std::vector<GraphEdge *> _parentEdges; // edges to preceding nodes 
     ChatBot *_chatBot;
@@ -44,7 +45,7 @@ public:
     // proprietary functions
     void AddToken(std::string token); // add answers to list
     void AddEdgeToParentNode(GraphEdge *edge);
-    void AddEdgeToChildNode(GraphEdge *edge);
+    void AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge);
 
     //// STUDENT CODE
     ////
